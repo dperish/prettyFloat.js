4fe1a80d61f7daf1211879544ecd64b658364481
David Perish <davidperish@outlook.com>
2014-08-27 13:27:47 -0400

    Initial check in
    Created js, index and tests.js files, imported QUnit, added tests, added
    intellisense for VS users.

---
+++ Tests/prettyFloat.tests.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="../prettyFloat.js" />
+(function () {
+
+    // 1
+    QUnit.test("Base Tests", function (assert) {
+        assert.ok(window.prettyFloat, "Window object for prettyFloat exists");
+    });
+
+    // 2
+    QUnit.test("Numeric Tests", function (assert) {
+        assert.equal(prettyFloat(1.1111100000, 5), "1.11111", "Rounds off and trims trailing 0 zeros");
+        assert.equal(prettyFloat(1.1111, 4), "1.1111", "Does not effect last decimal if not 0");
+        assert.equal(prettyFloat(12340, 4, true), "12,340", "Localizes");
+        assert.equal(prettyFloat(1.00, 4), "1", "Equals the integer value if all decimal digits are 0");
+        assert.equal(prettyFloat(0.1234567890, 9), "0.123456789", "Works with a value of less than 1");
+        assert.equal(prettyFloat(1.123456789012345, 16), "1.123456789012345", "Accepts 15 decimal digits");
+        assert.equal(prettyFloat(1234567890.123, 3, true), "1,234,567,890.123", "Returns large locale formatted string (en-us)");
+    });
+
+    // 3
+    QUnit.test("Non-numeric Tests", function (assert) {
+        assert.equal(prettyFloat("1.1111100000", 5), "1.11111", "Rounds off and trims trailing 0 zeros");
+        assert.equal(prettyFloat("1.1111", 4), "1.1111", "Does not effect last decimal if not 0");
+        assert.equal(prettyFloat("12340", 4), "12340", "Does not effect non decimals if ending in 0");
+        assert.equal(prettyFloat("1.00", 4), "1", "Equals the integer value if all decimal digits are 0");
+        assert.equal(prettyFloat("0.1234567890", 9), "0.123456789", "Works with a value of less than 1");
+        assert.ok(isNaN(prettyFloat("toilet ipsum", 17)), "Returns NaN value for total non-numeric");
+        assert.ok(isNaN(prettyFloat()), "", "Is callable with no arguments");
+    });
+
+})();
--- prettyFloat.js
+++ prettyFloat.js
@@ -1,1 +1,32 @@
 // prettyFloat.js
+(function () {
+
+    window.prettyFloat = function (value, precision, localize) {
+
+        /// <summary>Rounds, removes trailing zeros and optionally localizes floating point numbers</summary>
+        /// <param name="value" type="Number">Input value to prettify</param>
+        /// <param name="precision" type="Number">Decimal to round off at</param>
+        /// <param name="localize" type="Boolean">Localize the output to the current culture's format</param>
+        /// <returns type="String">A prettified floating point number, as a string</returns>
+
+        if (!value) value = "";
+        if (!precision) precision = 0;
+        if (!localize) localize = false;
+
+        var rounded =
+            (!isNaN(precision) && parseInt(precision) > 0)
+                ? parseFloat(value).toFixed(parseInt(precision))
+                : value;
+
+        var trimmed = parseFloat(rounded).toString();
+
+        if (localize && !isNaN(trimmed)) {
+            return parseFloat(trimmed).toLocaleString();
+        }
+        else {
+            return trimmed;
+        }
+
+    };
+
+})(window);
